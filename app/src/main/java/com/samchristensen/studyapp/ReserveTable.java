beb65bd5641902e702adebc1c7af6badd4dad11c
Deena Tenzer <deenatenzer@dyn-72-33-145-186.uwnet.wisc.edu>
2015-05-09 18:57:10 -0500

    sjghkxd

--- app/src/main/java/com/samchristensen/studyapp/ReserveTable.java
+++ app/src/main/java/com/samchristensen/studyapp/ReserveTable.java
@@ -1,25 +1,97 @@
 package com.samchristensen.studyapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.http.SslError;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 
 public class ReserveTable extends Activity {
 
+    private WebView mView;
+    private Button toApp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve_table);
 
-        String url = "https://library.ems.wis.edu/VirtualEMS_UW/";
-        WebView view = (WebView) findViewById(R.id.reservetable_webview);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
+        String url = "https://library.ems.wisc.edu/VirtualEMS_UW/";
+        mView = (WebView) findViewById(R.id.reservetable_webview);
+        mView.getSettings().setJavaScriptEnabled(true);
+        mView.getSettings().setDomStorageEnabled(true);
+        mView.setWebViewClient(new wvc() {
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error)
+            {
+                handler.proceed();
+            }
+        });
 
+        mView.loadUrl(url);
+
+        //Hookup Button to corresponding UI element and set clickaction
+        toApp = (Button) findViewById(R.id.toApp);
+        toApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reserve_table, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class wvc extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webview, String url) {
+            webview.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && mView.canGoBack()) {
+            mView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
 
 }
